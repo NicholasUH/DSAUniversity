a5967c79c9453093c869d355476c634c040adfca
Nicholas Tran <nicholastran2002@gmail.com>
2024-01-17 13:27:27 -0600

    topKFrequentElements

--- Arrays_Hashing/groupAnagrams.py
+++ Arrays_Hashing/groupAnagrams.py
@@ -1,13 +1,15 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        '''
-        create a hashmap to store sorted_word:anagrams
-        store each word by the sorted version
-        return the list of values
-        '''
+
         hashmap = collections.defaultdict(list)
         for word in strs:
             sorted_word = "".join(sorted(word))
             hashmap[sorted_word].append(word)
         
         return list(hashmap.values())
+    
+        '''
+        create a hashmap to store sorted_word:anagrams
+        store each word by the sorted version
+        return the list of values
+        '''
--- Arrays_Hashing/twoSum.py
+++ Arrays_Hashing/twoSum.py
@@ -1,5 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+
         hashmap = {}
 
         for index, num in enumerate(nums):
@@ -9,3 +10,6 @@ class Solution:
             else:
                 hashmap[num] = index
         
+'''
+
+'''
