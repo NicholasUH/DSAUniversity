08200929108b14aa9f0ba48e720dea2d34f6f75c
Nicholas Tran <nicholastran2002@gmail.com>
2024-05-06 13:39:24 -0500

    valid_anagram.py

---
+++ arrays_hashing/valid_anagram.py
@@ -0,0 +1,30 @@
+'''
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
+typically using all the original letters exactly once.
+'''
+
+'''
+Psuedo Code:
+1. check if the length of s and t are the same, indicating the possibility of an anagram
+2. create two dioctionaries for strings s and t
+3. add the letters and their counts to the respective dictionaries
+4. check if the dictionaries are the same
+- if they are being the same indicates that they both have the same letters and counts, therefore it is an anagram
+'''
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        if len(s) != len(t):
+            return False
+
+        s_dict = collections.defaultdict(int)
+        t_dict = collections.defaultdict(int)
+
+        for i in range(len(s)):
+            s_dict[s[i]] += 1
+            t_dict[t[i]] += 1
+
+        return s_dict == t_dict
