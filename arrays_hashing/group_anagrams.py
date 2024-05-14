c05bcde48570939318e80c66489977b6d0cd7095
Nicholas Tran <nicholastran2002@gmail.com>
2024-05-14 17:39:17 -0500

    group anagrams fix

--- arrays_hashing/group_anagrams.py
+++ arrays_hashing/group_anagrams.py
@@ -1,4 +1,5 @@
 '''
+Problem:
 Given an array of strings strs, group the anagrams together. 
 You can return the answer in any order.
 
@@ -7,6 +8,15 @@ different word or phrase, typically using all the original letters exactly once.
 '''
 
 '''
+Logic:
+To group anagrams together, we will create a dictionary to store a key:list of anagrams.
+The key that we will use will be tuple of character counts of each word. To create this tuple, we will create a list
+of length 26, for a-z, then iterate through each word and add the character count of each word to a list.
+We will then use the list as the key in the dictionary and add the word to the list associated with the key.
+'''
+
+'''
+Psuedo-Code:
 1.create a dictionary mapping character_counts : list of words
 2. iterate through strs and repeat the steps
 - calculate character count list using ascii values and indicies, a being index 0 to z being index 25
