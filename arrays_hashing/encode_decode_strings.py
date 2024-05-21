8bd19eb39c117ffe6e9705371e6cd40f275a8a8e
Nicholas Tran <nicholastran2002@gmail.com>
2024-05-20 21:03:50 -0500

    encode_decode

--- arrays_hashing/encode_decode_strings.py
+++ arrays_hashing/encode_decode_strings.py
@@ -3,3 +3,48 @@ Design an algorithm to encode a list of strings to a single string. The encoded 
 
 Please implement encode and decode
 '''
+
+'''
+Encode Logic:
+Iterate through strs and append the the length of the string and a delimiter symbol then the string itself.
+'''
+
+'''
+Decode Logic:
+Have 2 pointers i and j, i will be the beginning of what we extract while j is the end.
+Iterate through the encoded string
+-set j equal to i and iterate j until the delimiter is found
+-the bounds of i and j will be the length of the string
+- set i = j + 1, the start of the string
+- set j = j + length of string, the end of the string
+-append the string to result
+set i to j + 1, the next number for the next string
+'''
+
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        encoded_string = ""
+
+        for s in strs:
+            encoded_string += str(len(s)) + "$" + s
+        
+        return encoded_string
+
+    def decode(self, s: str) -> List[str]:
+        result = []
+        i = 0
+
+        while i < len(s):
+            j = i
+            while s[j] != "$":
+                j += 1
+            length = int(s[i:j])
+            i = j + 1
+            j = j + length
+            result.append(s[i:j+1])
+
+            i = j + 1
+        
+        return result
+        
