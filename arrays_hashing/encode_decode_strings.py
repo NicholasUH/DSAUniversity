3ba49bbc9c79ef376640faf526f43457ce989ed3
Nicholas Tran <nicholastran2002@gmail.com>
2024-05-30 13:51:20 -0500

    encode/decode logic update

--- arrays_hashing/encode_decode_strings.py
+++ arrays_hashing/encode_decode_strings.py
@@ -9,11 +9,11 @@ Logic:
 This problem asks us to create two functions, one to encode a string and one to decode the encoded string. The trick behind this problem
 is finding a way to make sure that when decoding the string, we correctly guess where the word begins and ends. To solve this, when we encode
 the list of strings together, we will add the length of the word in front of the word. We will also add a delimiter symbol as numbers could also
-be apart of the words. The list of words should now be one string for decoding. Now when we decode the string, we will have a begin and end iterator.
-The begin iterator will start at 0, while the end iterator will always start where begin is when iterating through the string. The end iterator
-will iterate until the delimiter symbol is found. Once it is found, we can find the length of the word at the begin iterator. Update the begin
-to the end + 1 where the word starts, and the end to end + length where the word ends. Append the string at those bounds to the result list.
-Update begin to the end + 1 to move to the next word length.
+be apart of the words. The list of words should now be one string for decoding. Now when we decode the string, we will have two iterators, i and j,
+which will help us to iterate through the string and find the length of the word and then find the word. The i iterator will always start
+at the index that contains the length, and j will start at the next index. j will look for the delimiter symbol to know when to stop. The two 
+iterators will then update to the begin and end of the word. For both functions, the time complexity is O(n) as we need to iterate through
+the list of strings and the encoded string for each respective function.
 '''
 
 class Solution:
