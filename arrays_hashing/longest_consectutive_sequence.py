8a107d4c728fbeca7d4888014ef1233e3c410bc0
Nicholas Tran <nicholastran2002@gmail.com>
2024-05-30 14:39:29 -0500

    lcs update logic

--- arrays_hashing/longest_consectutive_sequence.py
+++ arrays_hashing/longest_consectutive_sequence.py
@@ -7,11 +7,10 @@ You must write an algorithm that runs in O(n) time.
 
 '''
 Logic:
-Create a hashset containing the unique values of nums.
-Iterate through that hashset and at each element,
--see if it is the first in a sequence
--if so, keep checking if the next numbers in the sequence exists
-Update longest if a new longest is found
+For this problem, we need to find the longest consectutive sequence. One solution is to sort the array and then iterate through it, but sorting
+takes O(nlogn) time complexity. A better solution is to use a hashset to remove duplicates and have a constant time complexity for accessing
+elements. We will iterate through all the numbers in the hashset and for each number, check if it is the first in a sequence, meaning the number - 1
+does not exists. If so, see how long of a sequence can be made starting from that number and update the longest length. This solution will take O(n) time but have O(n) space.
 '''
 
 class Solution:
