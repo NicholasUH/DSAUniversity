5d7a9a3e386c723077722841d31721bff2d2d288
Nicholas Tran <nicholastran2002@gmail.com>
2024-05-30 13:47:18 -0500

    encode/decode logic update

--- arrays_hashing/encode_decode_strings.py
+++ arrays_hashing/encode_decode_strings.py
@@ -5,20 +5,15 @@ Please implement encode and decode
 '''
 
 '''
-Encode Logic:
-Iterate through strs and append the the length of the string and a delimiter symbol then the string itself.
-'''
-
-'''
-Decode Logic:
-Have 2 pointers i and j, i will be the beginning of what we extract while j is the end.
-Iterate through the encoded string
--set j equal to i and iterate j until the delimiter is found
--the bounds of i and j will be the length of the string
-- set i = j + 1, the start of the string
-- set j = j + length of string, the end of the string
--append the string to result
-set i to j + 1, the next number for the next string
+Logic:
+This problem asks us to create two functions, one to encode a string and one to decode the encoded string. The trick behind this problem
+is finding a way to make sure that when decoding the string, we correctly guess where the word begins and ends. To solve this, when we encode
+the list of strings together, we will add the length of the word in front of the word. We will also add a delimiter symbol as numbers could also
+be apart of the words. The list of words should now be one string for decoding. Now when we decode the string, we will have a begin and end iterator.
+The begin iterator will start at 0, while the end iterator will always start where begin is when iterating through the string. The end iterator
+will iterate until the delimiter symbol is found. Once it is found, we can find the length of the word at the begin iterator. Update the begin
+to the end + 1 where the word starts, and the end to end + length where the word ends. Append the string at those bounds to the result list.
+Update begin to the end + 1 to move to the next word length.
 '''
 
 class Solution:
