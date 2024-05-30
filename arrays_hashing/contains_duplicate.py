86ed5c8122bb660b5171262e97f6b5a0bfbcb3ad
Nicholas Tran <nicholastran2002@gmail.com>
2024-05-30 13:14:39 -0500

    contains duplicate logic rework

--- arrays_hashing/contains_duplicate.py
+++ arrays_hashing/contains_duplicate.py
@@ -5,8 +5,11 @@ Given an integer array nums, return true if any value appears at least twice in 
 
 '''
 Logic:
-Using the hashset, we can store elements we have already seen as we iterate through nums.
-Iterate through nums, check if already seen and do appropriate action. Either, return True or add element to hashset.
+For this problem, our goal is to find the duplicate element efficiently. The traditional way to solve this problem is brute forcing using two
+for-loops and individually check each element with the others. This is very inefficient as the time complexity is O(n^2). 
+A better solution is to use hashset. We can use the set as a way to store already seen elements and have efficent access to them. 
+The approach is to iterate through the array and add each element to the set. We also check if the element is already in the set. 
+If so, return True else return False. The time complexity of this solution is O(n) which is faster than the brute force, but does require O(n) space.
 '''
 
 class Solution:
