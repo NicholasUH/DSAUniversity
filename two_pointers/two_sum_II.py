ceb2ea3a670057610d5926ffd9762af808bd19f8
Nicholas Tran <nicholastran2002@gmail.com>
2024-05-31 00:38:30 -0500

    two sum 2 update logic

--- two_pointers/two_sum_II.py
+++ two_pointers/two_sum_II.py
@@ -1,10 +1,21 @@
 '''
-create a left and right pointer that starts at the first and last index respectively
-keep iterating the left and right towards each other and repeat steps
--find the sum of values at pointers
-- if sum < target, then we increase left pointer, increasing current sum closer to target
-- if sum > target, then we decrease right pointers, decreasing current sum closer to target
--else return [left, right] + 1, as 1-indexed
+Problem:
+Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+
+Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+
+The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+Your solution must use only constant extra space.
+'''
+
+'''
+Logic:
+Like the original two sum problem, we need to find the two numbers that add up to the target. This problem adds in the stipulation that the
+array is sorted. We can use two pointers starting at the begin and end of the array to find the two numbers that add up to the target. We will
+iterate until the pointers meet or cross. We will add the two numbers together at the pointers and check if it is equal to the target.
+To determine how to iterate the pointers, we will use the fact that the array is sorted to our advantage. When the bound move to the left,
+we know that the sum will get smaller, and vice versa when the bounds moves to the right due to the sorted array.
 '''
 
 class Solution:
